c3198a1b30ea7203862656cf69480210f2b9c215
kkt9253 <kkt9253@naver.com>
2025-07-25 01:25:45 +0900

    FEAT : Redis 설정 클래스, 의존성 추가 및 설정파일 수정
    - Lettuce 기반 RedisConnectionFactory 구성
    - JWT, Redis 의존성 추가
    - 설정파일 수정

--- src/main/java/likelion13/youcandoittoo/global/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package likelion13.youcandoittoo.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
