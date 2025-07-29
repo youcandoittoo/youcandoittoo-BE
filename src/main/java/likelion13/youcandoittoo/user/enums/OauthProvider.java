8b71ca649c2b03c68ece51ceff5a6d9c3ace955f
kkt9253 <kkt9253@naver.com>
2025-07-30 01:34:42 +0900

    FEAT : OAuth2 소셜 로그인 구현 (Local 로그인 고려)
    - 동일 이메일로 등록된 계정이 있을 경우:
      - 다른 소셜 계정이 있다면 해당 공급자로 로그인 유도
      - 일반 계정이 있다면 계정 통합 처리
    
    - 관련 DTO, Enum, Entity, Repository 클래스 추가

---
+++ src/main/java/likelion13/youcandoittoo/user/enums/OauthProvider.java
@@ -0,0 +1,7 @@
+package likelion13.youcandoittoo.user.enums;
+
+public enum OauthProvider {
+    NAVER,
+    GOOGLE,
+    KAKAO
+}
---
+++ src/main/java/likelion13/youcandoittoo/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package likelion13.youcandoittoo.user.repository;
+
+import likelion13.youcandoittoo.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
